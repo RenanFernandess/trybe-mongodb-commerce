3286381295b5cd0854e0cf010c887d27846ff501
Renan Fernandes <renzinestuods@gmail.com>
2023-02-08 20:51:52 -0300

    :sparkles: feat: Adiciona a quantidade de vendas dos sanduíches por dia da semana

---
+++ challenges/desafio22.js
@@ -0,0 +1,27 @@
+db.produtos.updateMany({}, {
+  $set: {
+    vendasPorDia: [0, 0, 0, 0, 0, 0, 0],
+  },
+});
+
+db.produtos.updateOne({
+  nome: "Big Mac",
+}, {
+  $inc: {
+    "vendasPorDia.3": NumberInt(60),
+  },
+});
+
+db.produtos.updateMany({
+  tags: { $all: ["bovino"] },
+}, {
+  $inc: {
+    "vendasPorDia.6": NumberInt(120),
+  },
+});
+
+db.produtos.find({}, {
+  _id: 0,
+  nome: 1,
+  vendasPorDia: 1,
+});
